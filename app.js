ff40dc35db81f11e7277104bea9c6391ce26c7bc
sibbe23 <sibbe23@gmail.com>
2023-02-16 20:32:54 +0530

    Connect Node

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
+const db = require('./util/database')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,6 +12,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products').then().catch();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,3 +24,14 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(3000);
+
+
+// CREATE TABLE `sys`.`products` (
+//     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+//     `title` VARCHAR(255) NOT NULL,
+//     `price` DOUBLE NOT NULL,
+//     `description` TEXT NOT NULL,
+//     `imageUrl` VARCHAR(255) NOT NULL,
+//     PRIMARY KEY (`id`),
+//     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+  
